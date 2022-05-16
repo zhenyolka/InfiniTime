48262c329ba96f84b802894c61670ceeb163df88
ITCactus <n/a>
2022-05-16 12:05:14 +0200

    post rebase fixes

--- src/displayapp/screens/settings/SettingDisplay.cpp
+++ src/displayapp/screens/settings/SettingDisplay.cpp
@@ -1,54 +1,78 @@
-#include "displayapp/screens/settings/SettingWatchFace.h"
+#include "displayapp/screens/settings/SettingDisplay.h"
 #include <lvgl/lvgl.h>
 #include "displayapp/DisplayApp.h"
-#include "displayapp/screens/CheckboxList.h"
-#include "displayapp/screens/Screen.h"
+#include "displayapp/Messages.h"
 #include "displayapp/screens/Styles.h"
+#include "displayapp/screens/Screen.h"
 #include "displayapp/screens/Symbols.h"
-#include "components/settings/Settings.h"
 
 using namespace Pinetime::Applications::Screens;
 
-constexpr const char* SettingWatchFace::title;
-constexpr const char* SettingWatchFace::symbol;
-
-SettingWatchFace::SettingWatchFace(Pinetime::Applications::DisplayApp* app, Pinetime::Controllers::Settings& settingsController)
-  : Screen(app),
-    settingsController {settingsController},
-    screens {app,
-             settingsController.GetWatchfacesMenu(),
-             {
-               [this]() -> std::unique_ptr<Screen> {
-                 return CreateScreen1();
-               },
-               [this]() -> std::unique_ptr<Screen> {
-                 return CreateScreen2();
-               }
-             },
-             Screens::ScreenListModes::UpDown} {
+namespace {
+  void event_handler(lv_obj_t* obj, lv_event_t event) {
+    auto* screen = static_cast<SettingDisplay*>(obj->user_data);
+    screen->UpdateSelected(obj, event);
+  }
 }
 
-SettingWatchFace::~SettingWatchFace() {
+constexpr std::array<uint16_t, 4> SettingDisplay::options;
+
+SettingDisplay::SettingDisplay(Pinetime::Applications::DisplayApp* app, Pinetime::Controllers::Settings& settingsController)
+  : Screen(app), settingsController {settingsController} {
+
+  lv_obj_t* container1 = lv_cont_create(lv_scr_act(), nullptr);
+
+  lv_obj_set_style_local_bg_opa(container1, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_OPA_TRANSP);
+  lv_obj_set_style_local_pad_all(container1, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 10);
+  lv_obj_set_style_local_pad_inner(container1, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 5);
+  lv_obj_set_style_local_border_width(container1, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 0);
+
+  lv_obj_set_pos(container1, 10, 60);
+  lv_obj_set_width(container1, LV_HOR_RES - 20);
+  lv_obj_set_height(container1, LV_VER_RES - 50);
+  lv_cont_set_layout(container1, LV_LAYOUT_COLUMN_LEFT);
+
+  lv_obj_t* title = lv_label_create(lv_scr_act(), nullptr);
+  lv_label_set_text_static(title, "Display timeout");
+  lv_label_set_align(title, LV_LABEL_ALIGN_CENTER);
+  lv_obj_align(title, lv_scr_act(), LV_ALIGN_IN_TOP_MID, 10, 15);
+
+  lv_obj_t* icon = lv_label_create(lv_scr_act(), nullptr);
+  lv_obj_set_style_local_text_color(icon, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_ORANGE);
+  lv_label_set_text_static(icon, Symbols::sun);
+  lv_label_set_align(icon, LV_LABEL_ALIGN_CENTER);
+  lv_obj_align(icon, title, LV_ALIGN_OUT_LEFT_MID, -10, 0);
+
+  char buffer[12];
+  for (unsigned int i = 0; i < options.size(); i++) {
+    cbOption[i] = lv_checkbox_create(container1, nullptr);
+    sprintf(buffer, "%3d seconds",  options[i] / 1000);
+    lv_checkbox_set_text(cbOption[i], buffer);
+    cbOption[i]->user_data = this;
+    lv_obj_set_event_cb(cbOption[i], event_handler);
+    SetRadioButtonStyle(cbOption[i]);
+
+    if (settingsController.GetScreenTimeOut() == options[i]) {
+      lv_checkbox_set_checked(cbOption[i], true);
+    }
+  }
+}
+
+SettingDisplay::~SettingDisplay() {
   lv_obj_clean(lv_scr_act());
   settingsController.SaveSettings();
 }
 
-bool SettingWatchFace::OnTouchEvent(Pinetime::Applications::TouchEvents event) {
-  return screens.OnTouchEvent(event);
-}
-
-std::unique_ptr<Screen> SettingWatchFace::CreateScreen1() {
-  std::array<const char*, 4> watchfaces {" Digital face", " Analog face", " PineTimeStyle", " Terminal"};
-  return std::make_unique<Screens::CheckboxList>(0, 2, app, settingsController, title,
-                                                 symbol, &Controllers::Settings::SetClockFace,
-                                                 &Controllers::Settings::GetClockFace,
-                                                 watchfaces);
-}
-
-std::unique_ptr<Screen> SettingWatchFace::CreateScreen2() {
-  std::array<const char*, 4> watchfaces {" Casio G7710", "", "", ""};
-  return std::make_unique<Screens::CheckboxList>(1, 2, app, settingsController, title,
-                                                 symbol, &Controllers::Settings::SetClockFace,
-                                                 &Controllers::Settings::GetClockFace,
-                                                 watchfaces);
+void SettingDisplay::UpdateSelected(lv_obj_t* object, lv_event_t event) {
+  if (event == LV_EVENT_CLICKED) {
+    for (unsigned int i = 0; i < options.size(); i++) {
+      if (object == cbOption[i]) {
+        lv_checkbox_set_checked(cbOption[i], true);
+        settingsController.SetScreenTimeOut(options[i]);
+        app->PushMessage(Applications::Display::Messages::UpdateTimeOut);
+      } else {
+        lv_checkbox_set_checked(cbOption[i], false);
+      }
+    }
+  }
 }
